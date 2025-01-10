450a2e6ea480f4e1c14f4d23df8fdfe9aba20b4d
ErfanEmami <erfan.emami123@gmail.com>
2025-01-10 12:53:42 -0700

    update to vite config

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  cacheDir: './.vite-cache', // Moves the cache to .vite-cache in the root of the frontend folder
 })
