2bb7ec0fe8541e1472f9e00dc43b83b170478a91
ErfanEmami <erfan.emami123@gmail.com>
2025-01-13 12:07:38 -0700

    frontend: skeleton set up

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,19 +1,25 @@
-import { Routes, Route } from 'react-router-dom';
-import { Home } from './pages/home';
-import { Summary } from './pages/summary';
-import { NotFound } from './pages/not_found';
+import { Routes, Route } from "react-router-dom";
+import { Home } from "./pages/home";
+import { Summary } from "./pages/summary";
+import { NotFound } from "./pages/not_found";
+
+const MAX_WIDTH = "1400px";
 
 const App = () => {
   return (
-    <div>
-      <nav>
-        <a href="/">Home</a> | <a href="/summary">Summary</a>
+    <div className="h-screen flex flex-col">
+      <nav className="p-4 border-b">
+        <div className={`m-auto max-w-[${MAX_WIDTH}]`}>
+          <a href="/">Home</a> | <a href="/summary">Summary</a>
+        </div>
       </nav>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/Summary" element={<Summary />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <div className={`flex-grow m-auto w-full max-w-[${MAX_WIDTH}]`}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Summary" element={<Summary />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
     </div>
   );
 };
