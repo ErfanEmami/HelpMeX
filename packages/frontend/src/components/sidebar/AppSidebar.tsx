221beaf4aae197e13ee550d404812664a6067761
ErfanEmami <erfan.emami123@gmail.com>
2025-01-14 14:15:28 -0700

    wip(frontend): adds sidebar

--- packages/frontend/src/components/sidebar/AppSidebar.tsx
+++ packages/frontend/src/components/sidebar/AppSidebar.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import * as React from "react";
 
 import { IdeaSwitcher } from "@/components/sidebar/IdeaSwitcher";
@@ -11,25 +9,36 @@ import {
   SidebarContent,
   SidebarFooter,
   SidebarHeader,
+  SidebarProvider,
   SidebarRail,
+  SidebarTrigger,
+  SidebarInset,
 } from "@/components/ui/sidebar";
 import { CRM_DEF, HELP_DEF, MARKET_ANALYSIS_DEF } from "./NavItems";
 
-export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+export function AppSidebar({ children, ...props }: React.ComponentProps<typeof Sidebar>) {
   return (
-    <Sidebar collapsible="icon" {...props}>
-      <SidebarHeader>
-        <IdeaSwitcher />
-      </SidebarHeader>
-      <SidebarContent>
-        <NavMain def={MARKET_ANALYSIS_DEF} />
-        <NavMain def={CRM_DEF} />
-        <NavMuted def={HELP_DEF} className="mt-auto" />
-      </SidebarContent>
-      <SidebarFooter>
-        <NavUser />
-      </SidebarFooter>
-      <SidebarRail />
-    </Sidebar>
+    <SidebarProvider defaultOpen={true}>
+      <Sidebar collapsible="icon" {...props}>
+        <SidebarHeader>
+          {/* <IdeaSwitcher /> */}
+        </SidebarHeader>
+        <SidebarContent>
+          <NavMain def={MARKET_ANALYSIS_DEF} />
+          <NavMain def={CRM_DEF} />
+          <NavMuted def={HELP_DEF} className="mt-auto" />
+        </SidebarContent>
+        <SidebarFooter>
+          <NavUser />
+        </SidebarFooter>
+        <SidebarRail />
+      </Sidebar>
+      <SidebarInset>
+        <div className="-mb-2 p-2">
+          <SidebarTrigger />
+        </div>
+        {children}
+      </SidebarInset>
+    </SidebarProvider>
   );
 }
