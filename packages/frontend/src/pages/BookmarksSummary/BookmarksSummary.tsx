69b5419dd4f090cbad9d34049beba01f26da78f7
ErfanEmami <erfan.emami123@gmail.com>
2025-01-18 13:59:52 -0700

    fix(frontend:BookmarksSummary): fixes rerendering issue

--- packages/frontend/src/pages/BookmarksSummary/BookmarksSummary.tsx
+++ packages/frontend/src/pages/BookmarksSummary/BookmarksSummary.tsx
@@ -29,54 +29,46 @@ export const BookmarksSummary = () => {
     );
   };
 
-  const BookmarksPanel = () => (
-    <ControlPanel title="Bookmarks" half isLoading={isLoadingBookmarks}>
-      <Control>
-        <div className="flex pb-2 gap-2 overflow-x-auto">
-          {authors.map((author) => (
-            <AuthorPill
-              {...author}
-              key={author.id}
-              selected={isSelected(author.id)}
-              onClick={() => handlePillClick(author.id)}
-            />
-          ))}
-        </div>
-      </Control>
-      <Content>
-        {bookmarks.map((bookmark) => (
-          <BookmarkCard key={bookmark.id} {...bookmark} />
-        ))}
-      </Content>
-    </ControlPanel>
-  );
-
-  const SummaryPanel = () => (
-    <ControlPanel title="Summary" half>
-      <Content>
-        {isLoadingSummary ? (
-          <Loading />
-        ) : bookmarksSummary ? (
-          bookmarksSummary.themes.map((theme) => <SummaryCard {...theme} />)
-        ) : null}
-      </Content>
-      <Control>
-        <Button
-          size="full"
-          disabled={isLoadingSummary}
-          onClick={fetchBookmarksSummary}
-        >
-          Get Summary
-        </Button>
-      </Control>
-    </ControlPanel>
-  );
-
   return (
     <Page>
       <div className="flex flex-1 overflow-y-hidden">
-        <BookmarksPanel />
-        <SummaryPanel />
+        <ControlPanel title="Bookmarks" half isLoading={isLoadingBookmarks}>
+          <Control>
+            <div className="flex pb-2 gap-2 overflow-x-auto">
+              {authors.map((author) => (
+                <AuthorPill
+                  {...author}
+                  key={author.id}
+                  selected={isSelected(author.id)}
+                  onClick={() => handlePillClick(author.id)}
+                />
+              ))}
+            </div>
+          </Control>
+          <Content>
+            {bookmarks.map((bookmark) => (
+              <BookmarkCard key={bookmark.id} {...bookmark} />
+            ))}
+          </Content>
+        </ControlPanel>
+        <ControlPanel title="Summary" half>
+          <Content>
+            {isLoadingSummary ? (
+              <Loading />
+            ) : bookmarksSummary ? (
+              bookmarksSummary.themes.map((theme) => <SummaryCard {...theme} />)
+            ) : null}
+          </Content>
+          <Control>
+            <Button
+              size="full"
+              disabled={isLoadingSummary}
+              onClick={fetchBookmarksSummary}
+            >
+              Get Summary
+            </Button>
+          </Control>
+        </ControlPanel>
       </div>
     </Page>
   );
