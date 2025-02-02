b0d368a1d4ecad789d15aac615ae6d4e5e6a68bb
ErfanEmami <erfan.emami123@gmail.com>
2025-02-02 12:45:44 -0700

    feat(backend): encrypts user refreshToken

--- packages/backend/lib/utils.js
+++ packages/backend/lib/utils.js
@@ -1,4 +1,5 @@
 import { TwitterApi } from "twitter-api-v2";
+import crypto from "crypto";
 
 export const validateResponse = (zodSchema, response) => {
   const validationResult = zodSchema.safeParse(response);
@@ -14,30 +15,56 @@ export const validateResponse = (zodSchema, response) => {
   return validationResult.data;
 };
 
-export const refreshTwitterToken = async (user) => {
-  try {
+// for background jobs (post scheduler) that need valid accessToken
+export const getAccessToken = async (user) => {
+  // update accessToken if it's expired
+  if (Date.now() > user.expiresAt) {
+    console.log("Access token expired, refreshing...");
+
     const client = new TwitterApi({
       clientId: process.env.TWITTER_CLIENT_ID,
       clientSecret: process.env.TWITTER_CLIENT_SECRET,
     });
 
     const {
-      client: refreshedClient,
-      accessToken,
-      refreshToken,
+      accessToken: newAccessToken,
+      refreshToken: newRefreshToken,
       expiresIn,
-    } = await client.refreshOAuth2Token(user.refreshToken);
+    } = await client.refreshOAuth2Token(user.getDecryptedRefreshToken());
 
-    user.accessToken = accessToken;
-    user.refreshToken = refreshToken;
+    user.accessToken = newAccessToken;
+    user.refreshToken = newRefreshToken;
     user.expiresAt = Date.now() + expiresIn * 1000;
     await user.save();
 
-    return accessToken;
-  } catch (error) {
-    console.error("Token refresh failed:", error);
-    throw new Error(
-      "Twitter authentication failed. User may need to reauthorize."
-    );
+    return newAccessToken;
   }
-};
+
+  return user.accessToken;
+}
+
+const getEncryptionKeyBuffer = () => {
+  const ENCRYPTION_KEY = Buffer.from(process.env.ENCRYPTION_KEY, "hex");
+  if (ENCRYPTION_KEY.length !== 32) {
+    throw new Error("ENCRYPTION_KEY must be exactly 32 bytes (64 hex characters).");
+  }
+  return ENCRYPTION_KEY
+}
+
+export const encrypt = (text) => {
+  const iv = crypto.randomBytes(16);
+  const cipher = crypto.createCipheriv("aes-256-cbc", getEncryptionKeyBuffer(), iv);
+  let encrypted = cipher.update(text);
+  encrypted = Buffer.concat([encrypted, cipher.final()]);
+  return iv.toString("hex") + ":" + encrypted.toString("hex");
+}
+
+export const decrypt = (text) => {
+  const parts = text.split(":");
+  const iv = Buffer.from(parts.shift(), "hex");
+  const encryptedText = Buffer.from(parts.join(":"), "hex");
+  const decipher = crypto.createDecipheriv("aes-256-cbc", getEncryptionKeyBuffer(), iv);
+  let decrypted = decipher.update(encryptedText);
+  decrypted = Buffer.concat([decrypted, decipher.final()]);
+  return decrypted.toString();
+}
